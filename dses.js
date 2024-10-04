890ca14d13094119a21ed41b63a076254d7a0af8
srijansundaram <srijansundram@gmail.com>
2024-10-04 22:42:39 +0530

    new file:   dice-1.png 	new file:   dice-2.png 	new file:   dice-3.png 	new file:   dice-4.png 	new file:   dice-5.png 	new file:   dice-6.png 	new file:   dses.html 	new file:   dses.js 	renamed:    game.css -> guessnumber.css 	renamed:    game.html -> guessnumber.html 	renamed:    game.js -> guessnumber.js 	new file:   pig-game-flowchart.png 	new file:   piggame.css 	new file:   piggame.html 	new file:   piggame.js

---
+++ dses.js
@@ -0,0 +1,6 @@
+// We are just setting up our editor here and learning to use google, stackoverflow and mdn and how to do debugging
+// set up prettier formatter to format our code
+// added live server extension to avoid reloading page everytime
+// added node js as well for the same thing as above
+
+'use strict';
